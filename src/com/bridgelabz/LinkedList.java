e5baa89dfe0e6df9b10ebd557a3570c460760ca7
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-20 02:25:55 +0530

    [Kavyasree J Ganesh] Linkedlist problem

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -4,4 +4,26 @@ public class LinkedList<T> {
 
     Node<T> head;
     Node<T> tail;
+    public void prePend(T key) {
+        Node<T> newNode = new Node(key);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        printLinkedList();
+    }
+
+
+    public void printLinkedList(){
+        System.out.println("Linked list after operation is");
+        Node ptr = head;
+
+        while(ptr != null){
+            System.out.print(ptr.key + " ");
+            ptr = ptr.next;
+        }
+    }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,8 +1,11 @@
 package com.bridgelabz;
 
 public class LinkedListMain {
-
-    public static void main(String[] args) {
-        LinkedList<Integer> linkedList1 = new LinkedList<>();
-    }
+        public static void main(String[] args) {
+            LinkedList<Integer> linkedList1 = new LinkedList<>();
+            linkedList1.prePend(70);
+            linkedList1.prePend(30);
+            linkedList1.prePend(56);
+        }
 }
+
