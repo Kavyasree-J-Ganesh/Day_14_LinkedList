9f4ca0bc4db56ee7b9c481c1702b555bba9de770
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-20 23:48:18 +0530

    [Kavyasree J Ganesh] LinkedList problem

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -4,6 +4,24 @@ public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
 
+    public void searchAndInsert(T key, T search){
+        Node ptr = head;
+        Node<T> newNode = new Node(key);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        } else {
+            while (ptr.key != search && ptr.next != null){
+                ptr = ptr.next;
+            }
+
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+
+        }
+        printLinkedList();
+    }
+
     public void append(T key){         // appending  UC3
         Node ptr = head;
         Node<T> newNode = new Node(key);
